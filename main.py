7c1349de1d9aab99c6b689c78a2546477868e6a3
Aleks Tacconi <aleks.tacconi@gmail.com>
2025-02-08 10:31:28 +0000

    simple st app

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+class Application:
+    def __init__(self) -> None:
+        self.__data_frame = pd.DataFrame(
+            {
+                "first column": [1, 2, 3, 4],
+                "second column": [10, 20, 30, 40]
+            }
+        )
+
+    def main(self) -> None:
+        st.write("my table:")
+        st.write(self.__data_frame)
+
 def main() -> None:
-    ...
+    app = Application()
+    app.main()
 
 if __name__ == "__main__":
     main()

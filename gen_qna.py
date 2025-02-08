0e88b94137af3a34d983e881d3faf8fc135ed3df
Aleks Tacconi <aleks.tacconi@gmail.com>
2025-02-08 19:38:43 +0000

    sorta working, no promises

--- gen_qna.py
+++ gen_qna.py
@@ -3,19 +3,13 @@ import os
 
 
 def main() -> None:
-    qna = {}
-
     with open(os.path.join("uploaded_files", "questions.txt"), mode="r", encoding="utf-8") as f:
-        for note in f.readlines():
-            note = note.strip()
-            question, answer = generate_question_answer(note)
-
-            qna[question] = answer
+        note = "".join(f.readlines())
+        response = generate_question_answer(note)
 
 
     with open("qna.txt", mode="w", encoding="utf-8") as f:
-        for question, answer in qna.items():
-            f.write(f"{question.strip()}%%%%%{answer.strip()}\n")
+        f.write(response)
 
 
 if __name__ == "__main__":

c6580b8047bfa62c9dd5ed20b7408e183182b662
fawwaz1123 <fawwazhahmed@gmail.com>
2025-02-08 21:46:51 +0000

    notes

---
+++ notes.py
@@ -0,0 +1,24 @@
+def upload_txt(self):
+        
+        uploaded_file = st.file_uploader("Choose a file", type=["txt"])
+
+        if uploaded_file is not None:
+            st.success("File uploaded successfully!")
+            self.save_uploaded_file(uploaded_file)
+            content = self.read_file_content(uploaded_file)  # Capture content
+
+            # Proceed with OpenAI API call if content is not None
+            if content:
+                client = OpenAI(api_key = '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' )
+                response = client.chat.completions.create(model="gpt-4o-mini-2024-07-18", messages=[
+                    {"role": "system", "content": "You are a helpful assistant, generate summarised notes in markdown format"},
+                    {"role": "user", "content": content},
+                ])
+
+                notes = response.choices[0].message.content
+                st.title("Summarised Notes 📝")
+                st.markdown(notes)
+                
+
+            return uploaded_file
+        return None
